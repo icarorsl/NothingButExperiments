126fd047ab7ec775c5e11a4c812cf08c8358ebab
IcaroJosé <IcaroJos@ICARO_PC>
2017-07-29 16:36:22 +0100

    updating

--- KeepTrying/MineCampTurtle/Program.cs
+++ KeepTrying/MineCampTurtle/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,218 @@ namespace MineCampTurtle
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            TurtleChallenge turtleChallenge = new TurtleChallenge(File.ReadAllLines(args[0]), File.ReadAllLines(args[1]));
+            string[] output = turtleChallenge.RunTest();
+            for (int i = 0; i < output.Length; i++)
+            {
+                Console.WriteLine(string.Format("Sequence {0}: {1}", i, output[i]));
+            }
+            File.WriteAllLines(args[1], output);
+            Console.Read();
         }
+    }
+
+
+    /// <summary>
+    /// defines the directions
+    /// </summary>
+    public enum Direction
+    {
+        North,
+        South,
+        East,
+        West
+    }
+
+    /// <summary>
+    /// Defines the position(x,y) of the turtle and its direction
+    /// </summary>
+    public class Point
+    {
+        public int X;
+        public int Y;
+        public Direction Direction;
+
+        public static Point ParsePoint(string value)
+        {
+            Point position = new Point();
+            string[] values = value.Split(',');
+            position.X = int.Parse(values[0]);
+            position.Y = int.Parse(values[1]);
+
+            if (values.Length > 2)
+                position.Direction = (Direction)Enum.Parse(typeof(Direction), values[2]);
+
+            return position;
+        }
+    }
+
+    /// <summary>
+    /// Turtle Challenge main class
+    /// </summary>
+    public class TurtleChallenge
+    {
+        /// <summary>
+        /// Marix size
+        /// </summary>
+        private Point _matrixSize;
+
+        /// <summary>
+        /// boolean matrix defining where the mines are
+        /// </summary>
+        private bool[,] _mines;
+
+        /// <summary>
+        /// What is the current position of the Turtle
+        /// </summary>
+        private Point _currentPosition;
+
+        /// <summary>
+        /// What is the exit position
+        /// </summary>
+        private Point _exitPosition;
+
+        /// <summary>
+        /// What are the action to be performed
+        /// </summary>
+        private string[] _actions;
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="settings"></param>
+        /// <param name="actions"></param>
+        public TurtleChallenge(string[] settings, string[] actions)
+        {
+            _matrixSize = Point.ParsePoint(settings[0]);
+            _currentPosition = Point.ParsePoint(settings[1]);
+            _exitPosition = Point.ParsePoint(settings[2]);
+
+            //creating the matrix
+            _mines = new bool[_matrixSize.X, _matrixSize.Y];
+
+            //setting the mines
+            for (int i = 3; i < settings.Length; i++)
+            {
+                Point pointMine = Point.ParsePoint(settings[i]);
+                _mines[pointMine.X, pointMine.Y] = true;
+            }
+
+            _actions = actions;
+        }
+
+        /// <summary>
+        /// perform a move of the turtle
+        /// </summary>
+        /// <param name="newPosition"></param>
+        private bool Move(Point newPosition)
+        {
+            bool allowedToMove = false;
+            switch (_currentPosition.Direction)
+            {
+                case Direction.North:
+                    allowedToMove = _currentPosition.X == newPosition.X && _currentPosition.Y - 1 == newPosition.Y && _currentPosition.Y - 1 >= 0;
+                    break;
+                case Direction.South:
+                    allowedToMove = _currentPosition.X == newPosition.X && _currentPosition.Y + 1 == newPosition.Y && _currentPosition.Y + 1 < _matrixSize.Y;
+                    break;
+                case Direction.East:
+                    allowedToMove = _currentPosition.Y == newPosition.Y && _currentPosition.X + 1 == newPosition.X && _currentPosition.X + 1 < _matrixSize.X;
+                    break;
+                case Direction.West:
+                    allowedToMove = _currentPosition.Y == newPosition.Y && _currentPosition.X + 1 == newPosition.X && _currentPosition.X - 1 <= 0;
+                    break;
+            }
+
+            if (allowedToMove)
+            {
+                _currentPosition.X = newPosition.X;
+                _currentPosition.Y = newPosition.Y;
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Rotates the turtle
+        /// </summary>
+        /// <param name="direction"></param>
+        private void Rotate()
+        {
+            switch (_currentPosition.Direction)
+            {
+                case Direction.North:
+                    _currentPosition.Direction = Direction.East;
+                    break;
+                case Direction.South:
+                    _currentPosition.Direction = Direction.West;
+                    break;
+                case Direction.East:
+                    _currentPosition.Direction = Direction.South;
+                    break;
+                case Direction.West:
+                    _currentPosition.Direction = Direction.North;
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// performs all the action and returns the result
+        /// </summary>
+        public string[] RunTest()
+        {
+            if (_mines[_exitPosition.X, _exitPosition.Y])
+            {
+                Console.WriteLine("Exit and Mine cannot be in the same position.");
+            }
+            else
+            {
+                for (int i = 0; i < _actions.Length; i++)
+                {
+                    string action = _actions[i].Split(':')[0];
+
+                    if (action.StartsWith("m=", StringComparison.OrdinalIgnoreCase))
+                    {
+                        Point pointMove = Point.ParsePoint(_actions[i].Substring(2));
+
+                        if (Move(pointMove))
+                        {
+                            if (_mines[_currentPosition.X, _currentPosition.Y])
+                            {
+                                _actions[i] = action + ": Mine hit. End of Game!";
+                                break;
+                            }
+                            else
+                            {
+                                _actions[i] = action + ": Successful move!";
+                            }
+
+                            if (_currentPosition.X == _exitPosition.X && _currentPosition.Y == _exitPosition.Y)
+                            {
+                                _actions[i] = action + ": Exit Position found!";
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            _actions[i] = action + ": Incorrect move. Still in danger!";
+                        }
+                    }
+                    else
+                        if (_actions[i].StartsWith("r", StringComparison.OrdinalIgnoreCase))
+                    {
+                        Rotate();
+                        _actions[i] = action + ": Successfull move!";
+                    }
+                }
+            }
+
+            return _actions;
+        }
+
     }
 }
--- KeepTrying/SafestPlaceCubeWithBombs/Program.cs
+++ KeepTrying/SafestPlaceCubeWithBombs/Program.cs
@@ -1,15 +1,207 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SafestPlaceCubeWithBombs
 {
+    /// <summary>
+    /// defines a tridimentional position x,y,z
+    /// </summary>
+    struct CubePosition
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+
+        public int DistToSqrd(CubePosition other)
+        {
+            int dx = X - other.X;
+            int dy = Y - other.Y;
+            int dz = Z - other.Z;
+
+            return dx * dx + dy * dy + dz * dz;
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    struct Range
+    {
+        public int Begin { get; private set; }
+        public int End { get; private set; }        // Exclusive
+
+        public Range(int begin, int end) : this()
+        {
+            Begin = begin;
+            End = end;
+        }
+    }
+
     class Program
     {
+        /// <summary>
+        /// inclusive cube size
+        /// </summary>
+        private const int CubeSize = 1001;
+
+        /// <summary>
+        /// Check the exclusions and return the valid out of the intersections
+        /// </summary>
+        /// <param name="exclusions"></param>
+        /// <returns></returns>
+        static IEnumerable<int> PossibleValues(List<Range> exclusions)
+        {
+            int next = 0;
+            foreach (var e in exclusions)
+            {
+                if (e.End > next)
+                {
+                    for (int result = next; result < e.Begin; result++)
+                        yield return result;
+
+                    next = Math.Max(next, e.End);
+                }
+            }
+
+            for (int result = next; result < CubeSize; result++)
+                yield return result;
+        }
+
+        /// <summary>
+        /// Simulate the bombs in its positions in order to test the best sqrt
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="zBombs"></param>
+        /// <param name="sqrRoot"></param>
+        /// <returns></returns>
+        static List<Range> SimulateBombPositionsAndTest(int x, int y, List<CubePosition> zBombs, int sqrRoot)
+        {
+            List<Range> result = new List<Range>();
+
+            foreach (var b in zBombs)
+            {
+                int dx = b.X - x;
+                int dy = b.Y - y;
+
+                int xYDistSqr = dx * dx + dy * dy;
+
+                if (sqrRoot >= xYDistSqr)
+                {
+                    int maxDz = (int)Math.Sqrt(sqrRoot - xYDistSqr);
+
+                    Range newRange = new Range(b.Z - maxDz, b.Z + maxDz + 1);
+
+                    // The intersection of a bomb is symetric around its Z
+                    // Also, each bomb has Z >= all previous ones
+                    // Thus, if the Begin of the range is less than a previous Begin, the new range totally superceeds the previous range, 
+                    //  and the previous range can be removed
+                    for (int iExisting = result.Count - 1; iExisting >= 0; iExisting--)
+                    {
+                        if (newRange.Begin <= result[iExisting].Begin)
+                        {
+                            result.RemoveAt(iExisting);
+                        }
+                        else
+                        {
+                            // result is build up sorted by Begin
+                            // Thus, if Begin at iExisting is too small, there is no point continuing back through the list
+                            break;
+                        }
+                    }
+
+                    result.Add(newRange);
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Process the test
+        /// </summary>
+        /// <param name="bombs"></param>
+        /// <returns></returns>
+        static int ProcessTest(List<CubePosition> bombs)
+        {
+            int safestSquareRoot = 0;
+            CubePosition safestPlace = new CubePosition();
+
+            // Basic algorithm - each x = x1, y = y1 defines a line of CubeSize points for each Z
+            // Project each bomb with current best radius onto this line to find
+            //  section where the sphere around each bomb intersects
+            // Then, only need to check Z values not in these intersections
+
+            List<CubePosition> zBombs = bombs.OrderBy(b => b.Z).ToList();
+
+            //reading the inclusive cube, each x, y
+            //Simulate the position the bombs and test/find the safestplace           
+            for (int x = 0; x < CubeSize; x++)
+            {
+                for (int y = 0; y < CubeSize; y++)
+                {
+                    List<Range> exclusions = SimulateBombPositionsAndTest(x, y, zBombs, safestSquareRoot);
+
+                    foreach (int z in PossibleValues(exclusions))
+                    {
+                        CubePosition p = new CubePosition { X = x, Y = y, Z = z };
+
+                        int newSqrtValue = bombs.Select(b => b.DistToSqrd(p)).Min();
+                        
+                        if (newSqrtValue > safestSquareRoot)
+                        {
+                            safestSquareRoot = newSqrtValue;
+                            safestPlace = p;
+                        }
+                    }
+                }
+            }
+
+            return safestSquareRoot;
+        }
+
+        /// <summary>
+        /// Startup method
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            List<List<CubePosition>> tests = new List<List<CubePosition>>();
+
+            StreamReader inputReader = new StreamReader(args[0]);
+
+            //reading input parameters
+            int testCasesNumber = int.Parse(inputReader.ReadLine());
+
+            for (int i = 0; i < testCasesNumber; i++)
+            {
+                List<CubePosition> bombs = new List<CubePosition>();
+                int bombsNumber = int.Parse(inputReader.ReadLine());
+                for (int b = 0; b < bombsNumber; b++)
+                {
+                    string[] positions = inputReader.ReadLine().Split(' ');
+                    bombs.Add(new CubePosition
+                    {
+                        X = int.Parse(positions[0]),
+                        Y = int.Parse(positions[1]),
+                        Z = int.Parse(positions[2])
+                    });
+                }
+                tests.Add(bombs);
+            }
+
+            //calculating and printing the results
+            for (int i = 0; i < tests.Count(); i++)
+            {
+                int safestPlace = ProcessTest(tests[i]);
+                Console.WriteLine("TestCase #{0}: {1}", i+1, safestPlace);
+            }
+
+            Console.Read();
         }
     }
 }
